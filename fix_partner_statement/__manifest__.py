6171274316e300649c11c2b170eb8ad1273f06bb
primateuy <diego@primate.uy>
2025-06-16 15:25:26 -0300

    fix partner statement

---
+++ fix_partner_statement/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Fix Partner Statement",
+    "version": "17.0.1.0.1",
+    "category": "Accounting & Finance",
+    "summary": "Fix for OCA Financial Reports (Filter exchange differences)",
+    "author": "PrimateUY",
+    "website": "https://www.primate.uy/",
+    "license": "AGPL-3",
+    "depends": ["partner_statement"],
+    "data": [
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ fix_partner_statement/report/__init__.py
@@ -0,0 +1,1 @@
+from . import activity_statement, report_statement_common
